1dc0da3f1d0f87af1816881fb5df05ae08d47721
Vova-Nik <33543617+Vova-Nik@users.noreply.github.com>
2020-04-23 17:05:33 +0300

    top 1200 made

--- borders.js
+++ borders.js
@@ -3,20 +3,36 @@ function rend() {
     [].forEach.call(document.querySelectorAll("*"), function (a) { a.style.outline = "1px solid #" + (~~(Math.random() * (1 << 24))).toString(16) });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+
+function init() {
     let bBtn = document.createElement("div");
-    bBtn.innerHTML = "Get Borders";
+    bBtn.innerHTML = 'Show blocks';
     document.body.appendChild(bBtn);
     console.log("Created", bBtn)
-    bBtn.style.backgroundColor = "tomato";
-    bBtn.style.width = "160px";
-    bBtn.style.height = "32px"
+    bBtn.style.backgroundColor = "rgba(128, 32, 32, .8)";
+    bBtn.style.border = "2px solid blue";
+    bBtn.style.width = "150px";
+    bBtn.style.textAlign = "center";
+    bBtn.style.textShadow = "1px 1px 0px rgba(200,206,204,0.52)";
+   
+
     bBtn.style.padding = "0 12px";
-    bBtn.style.fontSize = "24px";
+    bBtn.style.fontSize = "20px";
     bBtn.style.position = "fixed";
     bBtn.style.top = "256px";
     bBtn.style.lef = "16px";
     bBtn.style.cursor = "pointer";
     bBtn.style.borderRadius = "6px";
+    bBtn.className = "rendBtn";
     bBtn.onclick = rend;
-});
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+//window.addEventListener(`resize`, event => {
+setInterval(() => {
+    console.log("width - ", window.screen.width);
+    let bBtn = document.querySelector(".rendBtn");
+    bBtn.innerHTML = `Show blocks  ${window.screen.width}`;
+}
+    , 600);
