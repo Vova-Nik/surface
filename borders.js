458aa80d1a5961384f07d9e5f8b9651d23d43ef8
Vova-Nik <vmaseich@gmail.com>
2020-04-27 06:57:03 +0300

    after rest

--- borders.js
+++ borders.js
@@ -3,27 +3,27 @@ function rend() {
     [].forEach.call(document.querySelectorAll("*"), function (a) { a.style.outline = "1px solid #" + (~~(Math.random() * (1 << 24))).toString(16) });
 }
 
-
 function init() {
     let bBtn = document.createElement("div");
     bBtn.innerHTML = 'Show blocks';
     document.body.appendChild(bBtn);
     console.log("Created", bBtn)
-    bBtn.style.backgroundColor = "rgba(128, 32, 32, .8)";
+    bBtn.style.backgroundColor = "rgba(210, 210, 210, .6)";
     bBtn.style.border = "2px solid blue";
     bBtn.style.width = "150px";
     bBtn.style.textAlign = "center";
-    bBtn.style.textShadow = "1px 1px 0px rgba(200,206,204,0.52)";
+    bBtn.style.textShadow = "1px 1px 0px rgba(240, 240, 240, 0.6)";
    
 
     bBtn.style.padding = "0 12px";
     bBtn.style.fontSize = "20px";
     bBtn.style.position = "fixed";
     bBtn.style.top = "256px";
-    bBtn.style.lef = "16px";
+    bBtn.style.left = "16px";
     bBtn.style.cursor = "pointer";
     bBtn.style.borderRadius = "6px";
     bBtn.className = "rendBtn";
+    bBtn.style.zIndex = "1000";
     bBtn.onclick = rend;
 }
 
@@ -33,11 +33,6 @@ document.addEventListener('DOMContentLoaded', init);
 setInterval(() => {
     console.log("width - ", window.screen.width);
     let bBtn = document.querySelector(".rendBtn");
-    // bBtn.innerHTML = `Show blocks  ${window.screen.width}`;
     bBtn.innerHTML = `Show blocks  ${window.innerWidth}`;
 }
     , 600);
-
-
-// alert( window.innerWidth ); // полная ширина окна
-// alert( document.documentElement.clientWidth ); 
